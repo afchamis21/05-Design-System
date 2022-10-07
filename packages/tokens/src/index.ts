7bea15195a5538e50ab1a3eefd82e3698148f865
André Chamis <andrechamis21@gmail.com>
2022-10-07 19:20:32 -0300

    Tokens package finalized

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,12 @@
+// import { colors } from '@ignite-ui-ac/tokens/colors -> 
+// import { colors } from '@ignite-ui-ac/tokens
+
+export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
